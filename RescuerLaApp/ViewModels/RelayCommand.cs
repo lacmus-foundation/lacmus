b801d54c25f7ce79417966aa686b65f57cfe3e69
gosha20777 <gosha20777@live.ru>
2019-04-06 21:56:06 +0300

    feat: init view-model and create alternative to System.Windows.Input.ComandManager

---
+++ RescuerLaApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using RescuerLaApp.Models;
+
+namespace RescuerLaApp.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute is null.");
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+            this.RaiseCanExecuteChangedAction = RaiseCanExecuteChanged;
+            SimpleCommandManager.AddRaiseCanExecuteChangedAction(ref RaiseCanExecuteChangedAction);
+        }
+
+        ~RelayCommand()
+        {
+            RemoveCommand();
+        }
+
+        public void RemoveCommand()
+        {
+            SimpleCommandManager.RemoveRaiseCanExecuteChangedAction(RaiseCanExecuteChangedAction);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+            SimpleCommandManager.RefreshCommandStates();
+        }
+
+        public bool CanExecute
+        {
+            get { return _canExecute == null || _canExecute(); }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+
+        private readonly Action RaiseCanExecuteChangedAction;
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- RescuerLaApp/ViewModels/ViewModelBase.cs
+++ RescuerLaApp/ViewModels/ViewModelBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using ReactiveUI;
 
@@ -7,5 +8,12 @@ namespace RescuerLaApp.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
