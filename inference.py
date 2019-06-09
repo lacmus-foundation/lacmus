5135071515c4a048734c8cf5f2ee5e451ab63adc
gosha20777 <gosha20777@live.ru>
2019-06-09 14:17:39 +0300

    better inference

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import keras
 # import keras_retinanet
+import tensorflow as tf
 from keras_retinanet import models
 from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
 from keras_retinanet.utils.visualization import draw_box, draw_caption
@@ -12,70 +13,91 @@ import sys
 import os
 import numpy as np
 import time
+from flask import Flask, jsonify, request, abort
 
-import tensorflow as tf
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'status': "server is running"}), 200
+
+@app.route('/exit')
+def stop():
+    exit()
+
+@app.route('/image', methods=['POST'])
+def predict_image():
+    if not request.json or not 'imagePatch' in request.json:
+        abort(400)
+    
+    
+    caption = run_detection_image(model, labels_to_names, request.json['imagePatch'])
+    return caption, 200
+
+
+def run_detection_image(model, labels_to_names, filepath):
+    print("start predict {}", filepath)
+    with graph.as_default():
+        image = read_image_bgr(filepath)
+
+        # copy to draw on
+        draw = image.copy()
+        draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+        # preprocess image for network
+        image = preprocess_image(image)
+        image, scale = resize_image(image)
+
+        # process image
+        start = time.time()
+        boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+
+        # correct for image scale
+        boxes /= scale
+        caption = ""
+
+        # visualize detections
+        for box, score, label in zip(boxes[0], scores[0], labels[0]):
+            # scores are sorted so we can break
+            if score < 0.5:
+                break
+            b = np.array(box.astype(int)).astype(int)
+            # x1 y1 x2 y2
+            caption += "output= {} {} {} {} {} {:.3f}\n".format(b[0], b[1], b[2], b[3], labels_to_names[label], score)
+        print("done {}", caption)
+        return caption
 
 def get_session():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    return tf.Session(config=config)
+    session = tf.Session(config=config)
+    return session
 
-
-def run_detection_image(model, labels_to_names, filepath):
-    image = read_image_bgr(filepath)
-
-    # copy to draw on
-    draw = image.copy()
-    draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
-
-    # preprocess image for network
-    image = preprocess_image(image)
-    image, scale = resize_image(image)
-
-    # process image
-    start = time.time()
-    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
-
-    # correct for image scale
-    boxes /= scale
-    caption = ""
-
-    # visualize detections
-    for box, score, label in zip(boxes[0], scores[0], labels[0]):
-        # scores are sorted so we can break
-        if score < 0.5:
-            break
-        b = np.array(box.astype(int)).astype(int)
-        # x1 y1 x2 y2
-        caption += "output= {} {} {} {} {} {:.3f}\n".format(b[0], b[1], b[2], b[3], labels_to_names[label], score)
-    return caption
-
-def main(args=None):
-    # parse arguments
-    #if args is None:
-    #    args = sys.argv[1:]
-    args = parse_args(args)
-
-    # use this environment flag to change which GPU to use
-    #os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-    # set the modified tf session as backend in keras
+def load_model(args):
     keras.backend.tensorflow_backend.set_session(get_session())
     model_path = args.model
     # load retinanet model
+    global model
     model = models.load_model(model_path, backbone_name='resnet50')
     # load label to names mapping for visualization purposes
+    global labels_to_names
     labels_to_names = {0: 'Pedestrian'}
-    print(run_detection_image(model, labels_to_names, args.image))
+    global graph
+    graph = tf.get_default_graph()
+    print('model loaded')
+    return model, labels_to_names
 
 def parse_args(args):
     """ Parse the arguments.
     """
-    parser     = argparse.ArgumentParser(description='Evaluation script for a RetinaNet network.')
-
+    parser = argparse.ArgumentParser(description='Evaluation script for a RetinaNet network.')
     parser.add_argument('--model',              help='Path to RetinaNet model.', default=os.path.join('snapshots', 'resnet50_liza_alert_v1_interface.h5'))
-    parser.add_argument('--image',              help='Path to image.', default='examples/235.jpg')
-
     return parser.parse_args(args)
+
+def main(args=None):
+    args = parse_args(args)
+    load_model(args)
+    app.run(debug=True, host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
