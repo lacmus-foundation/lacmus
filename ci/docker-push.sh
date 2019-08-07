0ed37033b5acd413acbf7c22650e6ffa74ee7830
Andrew Grigorev <andrew@ei-grad.ru>
2019-08-07 17:41:07 +0300

    travis: build tags and "-gpu" images

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+if [ -z "$SECRET_DOCKER_PASSWORD" ]; then
+   echo SECRET_DOCKER_PASSWORD environment variable is not set. Skipping docker push.
+   exit 0
+fi
+
 set -e
 set -x
 
+IMAGE=lizaalert/lacmus
+TAG=travis-${TRAVIS_COMMIT:0:7}${TARGET#build}
+
 docker login -u $SECRET_DOCKER_LOGIN -p $SECRET_DOCKER_PASSWORD
-docker push $TRAVIS_REPO_SLUG:travis-$TRAVIS_BUILD_NUMBER
-docker tag $TRAVIS_REPO_SLUG:travis-$TRAVIS_BUILD_NUMBER $TRAVIS_REPO_SLUG:$TRAVIS_BRANCH
-docker push $TRAVIS_REPO_SLUG:$TRAVIS_BRANCH
+docker push $IMAGE:$TAG
+
+if [ -n "$TRAVIS_TAG" ]; then
+    docker tag $IMAGE:$TAG $IMAGE:${TRAVIS_TAG}${TARGET#build}
+    docker push $IMAGE:$TRAVIS_TAG${TARGET#build}
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" && \
+     "$TRAVIS_REPO_SLUG" == "$IMAGE" && \
+     "$TRAVIS_BRANCH" == "master" ]; then
+    docker tag $IMAGE:$TAG $IMAGE:latest
+    docker push $IMAGE:latest
+fi
