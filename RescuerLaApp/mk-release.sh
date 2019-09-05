03dca72cfc8fee6c4a90bcac50780df772ac4999
gosha20777 <gosha20777@live.ru>
2019-09-05 16:59:04 +0300

    upd: 0.2.8 release

--- RescuerLaApp/mk-release.sh
+++ RescuerLaApp/mk-release.sh
@@ -3,9 +3,9 @@ rm -rf ./bin/app/ && rm -r ./bin/Release/
 echo "restoring packeges"
 dotnet restore
 echo -n "building for linux: ubuntu16"
-dotnet publish --framework netcoreapp2.2 --runtime="ubuntu.16.04-x64" -c Release -o ./bin/app/ununtu16
+dotnet publish --framework netcoreapp2.2 --runtime="ubuntu.16.04-x64" -c Release -o ./bin/app/ubuntu16
 echo -n "building for linux: ubuntu18"
-dotnet publish --framework netcoreapp2.2 --runtime="ubuntu.18.04-x64" -c Release -o ./bin/app/ununtu18
+dotnet publish --framework netcoreapp2.2 --runtime="ubuntu.18.04-x64" -c Release -o ./bin/app/ubuntu18
 echo -n "building for linux"
 dotnet publish --framework netcoreapp2.2 --runtime="linux-x64" -c Release -o ./bin/app/linux
 echo -n "building for win10"
@@ -13,8 +13,8 @@ dotnet publish --framework netcoreapp2.2 --runtime="win10-x64" -c Release -o ./b
 echo -n "building for osx"
 dotnet publish --framework netcoreapp2.2 --runtime="osx-x64" -c Release -o ./bin/app/osx
 cd ./bin/app/
-zip -r -9 ./ununtu16.zip ./ununtu16/
-zip -r -9 ./ununtu18.zip ./ununtu18/
+zip -r -9 ./ubuntu16.zip ./ubuntu16/
+zip -r -9 ./ubuntu18.zip ./ubuntu18/
 zip -r -9 ./linux.zip ./linux/
 zip -r -9 ./win10.zip ./win10/
 zip -r -9 ./osx.zip ./osx/
